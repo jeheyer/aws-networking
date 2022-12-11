17268a69871e9048b9eba217d2169ab6e229d881
John Heyer <jheyer@otldap.net>
2022-12-10 21:28:12 -0800

    require 1.3

--- aws_cloudfront/provider.tf
+++ aws_cloudfront/provider.tf
@@ -1,13 +1,4 @@
-terraform {
-  required_version = ">= 1.1.6"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "> 4.15.0"
-    }
-  }
+provider "aws" {
+  region = var.region
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
