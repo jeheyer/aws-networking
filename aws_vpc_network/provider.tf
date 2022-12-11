122d1f09902737c65e28229a0dd875559e11dcb7
John Heyer <jheyer@otldap.net>
2022-12-10 21:42:08 -0800

    require 1.3

--- aws_vpc_network/provider.tf
+++ aws_vpc_network/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.1.6"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "> 4.15.0"
-    }
-  }
-}
-
 provider "aws" {
   profile = "default"
   region  = var.vpc.region
